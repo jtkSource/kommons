da42959e4e11c17a2daf6810ff0e8d0a1fe9e0b6
AndresAusecha <daausecham@unal.edu.co>
2019-01-19 16:50:23 -0500

    switch to new deployment debug branch

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'oss-8-maven-deploy-debug' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P sign,build-extras --settings deployment/settings.xml
 fi
