d46b231b10fe9ba6f1bda14f6bd97c62355cc6fe
AndresAusecha <daausecham@unal.edu.co>
2019-02-21 07:06:38 -0500

    add new key

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'oss-8-maven-deploy-debug' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_c97290677436_key -iv $encrypted_c97290677436_iv -in keys.asc.enc -out keys.asc -d
-    gpg --fast-import keys.asc
+    openssl aes-256-cbc -K $encrypted_c97290677436_key -iv $encrypted_c97290677436_iv -in codesigning.asc.enc -out codesigning.asc -d
+    gpg --fast-import codesigning.asc
 fi
